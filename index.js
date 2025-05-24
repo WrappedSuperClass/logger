931acb675bb92c43f3b9e5737058847fbc2ea778
lukaspostulka <lukas.postulka@gmail.com>
2025-05-24 09:03:48 +0200

    Add ip adress to log

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors'); // Import the cors package
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('trust proxy', true); // Necessary to get the correct IP when behind a proxy like Railway
+
 // Middleware to parse JSON bodies
 app.use(express.json());
 
@@ -48,16 +50,18 @@ function sendEventToAllClients(eventData) {
 
 app.post("/log", (req, res) => {
   const message = req.body.message;
+  const ip = req.ip; // Get the client's IP address
   
   if (message === undefined) {
-    console.log("Received request with no message in body");
+    console.log(`[${ip}] Received request with no message in body`);
     return res.status(400).json({ success: false, message: "'message' field is missing in the request body" });
   }
   
-  console.log("Received message:", message);
+  console.log(`[${ip}] Received message:`, message);
   
   const newLogEntry = { 
     type: 'new_log', 
+    ip: ip, // Include IP in the log entry
     content: message, 
     timestamp: new Date().toISOString() 
   };
